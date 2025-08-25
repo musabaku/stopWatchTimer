c40ffb4dcbd4c76b64639129562b356b1c59cbad
Musab Ahmed Khan Umaır <musab@192.168.1.108>
2025-08-25 13:06:37 +0300

    time type added

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,9 +2,9 @@ import { Text } from "react-native";
 import { useContext } from "react";
 import { TimerContext } from "../context/TimerContext";
 export default function HomeScreen() {
-  const se = useContext(TimerContext)
-  // const {seconds,isRunning,startTimer,stopTimer,resetTimer} = useContext(TimerContext)
-  console.log(se)
+  // const se = useContext(TimerContext)
+  // // const {seconds,isRunning,startTimer,stopTimer,resetTimer} = useContext(TimerContext)
+  // console.log(se)
   return (
   <>
   <Text>Musab</Text>
--- app/context/TimerContext.tsx
+++ app/context/TimerContext.tsx
@@ -1,3 +1,18 @@
 import { createContext } from "react";
+type TimerContextType ={
+    seconds: number;
+    isRunning: boolean;
+    startTimer : ()=>void;
+    stopTimer : ()=>void;
+    resetTimer : ()=>void;
 
-export const TimerContext = createContext(null)
+
+}
+const defaultValue = {
+    seconds:0,
+    isRunning:false,
+    startTimer:()=>{},
+    stopTimer:()=>{},
+    resetTimer:()=>{},
+}
+export const TimerContext = createContext<TimerContextType>(defaultValue)
